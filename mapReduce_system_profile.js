86fd62eb9d0947fdb375755160be703f5ba00b35
Adam Pridmore <adam_pridmore@hotmail.com>
2014-11-03 16:04:46 +0000

    - Now uses the correct toQueryKeys.js passed into the map method via the scope parameter.

--- mapReduce_system_profile.js
+++ mapReduce_system_profile.js
@@ -1,24 +1,6 @@
+load('toQueryKeys.js');
+
 var map = function() {
-	var toQueryKeys = function(query){
-	    var keys = {};
-
-	    for(var keyName in query){
-	        var keyValue = query[keyName];
-
-	        if (keyName === "$in"){
-	            return {$in: 1};
-	        } else if (keyValue instanceof ObjectId || keyValue instanceof Date){
-	            keys[keyName] = 1;
-	        } else if (typeof(keyValue) === 'object'){
-	            keys[keyName] = toQueryKeys(keyValue);
-	        } else {
-	            keys[keyName] = 1;
-	        }
-	    }
-
-	    return keys;
-	};
-
 	var queryKeys = {};
 
 	if (this.query){
@@ -113,7 +95,8 @@ function runMapReduce(setting){
 		},
 		query: query,
 		scope : {
-			durationMs: durationMs
+			durationMs: durationMs,
+			toQueryKeys: toQueryKeys
 		},
 		finalize: finalize
 	};
